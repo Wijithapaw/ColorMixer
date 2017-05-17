172e5d788b97d736cc013431914968e54be36a62
Wijitha <wijithapaw@gmail.com>
2017-05-17 19:26:01 +0530

    initial commit

---
+++ ColorMixer/MainForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ColorMixer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddRow = new System.Windows.Forms.Button();
+            this.btnDelRow = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAddCol = new System.Windows.Forms.Button();
+            this.btnDelCol = new System.Windows.Forms.Button();
+            this.pnlColor1 = new System.Windows.Forms.Panel();
+            this.cbColor1 = new System.Windows.Forms.ComboBox();
+            this.cbColor2 = new System.Windows.Forms.ComboBox();
+            this.pnlColor2 = new System.Windows.Forms.Panel();
+            this.btnFinalize = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myPanel
+            // 
+            this.myPanel.BackColor = System.Drawing.Color.Transparent;
+            this.myPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.myPanel.ColumnCount = 1;
+            this.myPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.5F));
+            this.myPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.5F));
+            this.myPanel.Location = new System.Drawing.Point(83, 114);
+            this.myPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.myPanel.Name = "myPanel";
+            this.myPanel.RowCount = 1;
+            this.myPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.myPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.myPanel.Size = new System.Drawing.Size(870, 285);
+            this.myPanel.TabIndex = 0;
+            this.myPanel.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.myPanel_CellPaint);
+            this.myPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.myPanel_MouseClick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(984, 54);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 28);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddRow);
+            this.groupBox1.Controls.Add(this.btnDelRow);
+            this.groupBox1.Location = new System.Drawing.Point(83, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(89, 55);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rows";
+            // 
+            // btnAddRow
+            // 
+            this.btnAddRow.Location = new System.Drawing.Point(52, 21);
+            this.btnAddRow.Name = "btnAddRow";
+            this.btnAddRow.Size = new System.Drawing.Size(27, 23);
+            this.btnAddRow.TabIndex = 9;
+            this.btnAddRow.Text = "+";
+            this.btnAddRow.UseVisualStyleBackColor = true;
+            this.btnAddRow.Click += new System.EventHandler(this.btnAddRow_Click);
+            // 
+            // btnDelRow
+            // 
+            this.btnDelRow.Location = new System.Drawing.Point(9, 21);
+            this.btnDelRow.Name = "btnDelRow";
+            this.btnDelRow.Size = new System.Drawing.Size(27, 23);
+            this.btnDelRow.TabIndex = 8;
+            this.btnDelRow.Text = "-";
+            this.btnDelRow.UseVisualStyleBackColor = true;
+            this.btnDelRow.Click += new System.EventHandler(this.btnDelRow_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAddCol);
+            this.groupBox2.Controls.Add(this.btnDelCol);
+            this.groupBox2.Location = new System.Drawing.Point(194, 36);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(90, 55);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Columns";
+            // 
+            // btnAddCol
+            // 
+            this.btnAddCol.Location = new System.Drawing.Point(52, 21);
+            this.btnAddCol.Name = "btnAddCol";
+            this.btnAddCol.Size = new System.Drawing.Size(27, 23);
+            this.btnAddCol.TabIndex = 11;
+            this.btnAddCol.Text = "+";
+            this.btnAddCol.UseVisualStyleBackColor = true;
+            this.btnAddCol.Click += new System.EventHandler(this.btnAddCol_Click);
+            // 
+            // btnDelCol
+            // 
+            this.btnDelCol.Location = new System.Drawing.Point(9, 21);
+            this.btnDelCol.Name = "btnDelCol";
+            this.btnDelCol.Size = new System.Drawing.Size(27, 23);
+            this.btnDelCol.TabIndex = 10;
+            this.btnDelCol.Text = "-";
+            this.btnDelCol.UseVisualStyleBackColor = true;
+            this.btnDelCol.Click += new System.EventHandler(this.btnDelCol_Click);
+            // 
+            // pnlColor1
+            // 
+            this.pnlColor1.BackColor = System.Drawing.Color.Transparent;
+            this.pnlColor1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlColor1.Location = new System.Drawing.Point(984, 168);
+            this.pnlColor1.Name = "pnlColor1";
+            this.pnlColor1.Size = new System.Drawing.Size(80, 69);
+            this.pnlColor1.TabIndex = 8;
+            // 
+            // cbColor1
+            // 
+            this.cbColor1.FormattingEnabled = true;
+            this.cbColor1.Items.AddRange(new object[] {
+            "None",
+            "Red",
+            "Green",
+            "Yellow",
+            "Black",
+            "White"});
+            this.cbColor1.Location = new System.Drawing.Point(984, 114);
+            this.cbColor1.Name = "cbColor1";
+            this.cbColor1.Size = new System.Drawing.Size(80, 24);
+            this.cbColor1.TabIndex = 9;
+            this.cbColor1.SelectionChangeCommitted += new System.EventHandler(this.cbColor1_SelectedIndexChanged);
+            // 
+            // cbColor2
+            // 
+            this.cbColor2.FormattingEnabled = true;
+            this.cbColor2.Items.AddRange(new object[] {
+            "None",
+            "Red",
+            "Green",
+            "Yellow",
+            "Black",
+            "White"});
+            this.cbColor2.Location = new System.Drawing.Point(984, 267);
+            this.cbColor2.Name = "cbColor2";
+            this.cbColor2.Size = new System.Drawing.Size(80, 24);
+            this.cbColor2.TabIndex = 11;
+            this.cbColor2.SelectionChangeCommitted += new System.EventHandler(this.cbColor2_SelectionChangeCommitted);
+            // 
+            // pnlColor2
+            // 
+            this.pnlColor2.BackColor = System.Drawing.Color.Transparent;
+            this.pnlColor2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlColor2.Location = new System.Drawing.Point(984, 321);
+            this.pnlColor2.Name = "pnlColor2";
+            this.pnlColor2.Size = new System.Drawing.Size(80, 69);
+            this.pnlColor2.TabIndex = 10;
+            // 
+            // btnFinalize
+            // 
+            this.btnFinalize.Location = new System.Drawing.Point(877, 57);
+            this.btnFinalize.Name = "btnFinalize";
+            this.btnFinalize.Size = new System.Drawing.Size(75, 23);
+            this.btnFinalize.TabIndex = 12;
+            this.btnFinalize.Text = "Finalize";
+            this.btnFinalize.UseVisualStyleBackColor = true;
+            this.btnFinalize.Click += new System.EventHandler(this.btnFinalize_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 578);
+            this.Controls.Add(this.btnFinalize);
+            this.Controls.Add(this.cbColor2);
+            this.Controls.Add(this.pnlColor2);
+            this.Controls.Add(this.cbColor1);
+            this.Controls.Add(this.pnlColor1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.myPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "mainForm";
+            this.Text = "Saree Designer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel myPanel;
+        private System.Windows.Forms.Button btnReset;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddRow;
+        private System.Windows.Forms.Button btnDelRow;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAddCol;
+        private System.Windows.Forms.Button btnDelCol;
+        private System.Windows.Forms.Panel pnlColor1;
+        private System.Windows.Forms.ComboBox cbColor1;
+        private System.Windows.Forms.ComboBox cbColor2;
+        private System.Windows.Forms.Panel pnlColor2;
+        private System.Windows.Forms.Button btnFinalize;
+    }
+}
+
---
+++ ColorMixer/MainForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorMixer
+{
+    public partial class mainForm : Form
+    {
+        Dictionary<Point, Color> cellColors = new Dictionary<Point, Color>();
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectColor(Color color)
+        {
+            this.pnlColor1.BackColor = color;
+        }
+
+        private void AddColor(Color newColor)
+        {
+            Color existingColor = myPanel.BackColor;
+
+            double ratio = 0.5;
+
+            if (existingColor.Name.ToLower() == "transparent")
+                ratio = 1;
+
+            this.myPanel.BackColor = Utility.Blend(newColor, existingColor, ratio);
+        }
+               
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cellColors.Clear();
+            myPanel.RowCount = 1;
+            myPanel.ColumnCount = 1;
+            myPanel.BackColor = Color.Transparent;
+            cbColor1.SelectedIndex = 0;
+            cbColor2.SelectedIndex = 0;
+            myPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            pnlColor1.BackColor = Color.Transparent;
+            pnlColor2.BackColor = Color.Transparent;
+        }
+
+        private void btnDelCol_Click(object sender, EventArgs e)
+        {
+            if (myPanel.ColumnCount > 1)
+            {
+                ColumnStyle colStyle = myPanel.ColumnStyles[myPanel.ColumnCount - 1];
+
+                myPanel.ColumnCount--;
+
+                myPanel.ColumnStyles.RemoveAt(myPanel.ColumnCount - 1);
+            }
+        }
+
+        private void btnAddCol_Click(object sender, EventArgs e)
+        {
+            ColumnStyle colStyle = myPanel.ColumnStyles[myPanel.ColumnCount - 1];
+
+            myPanel.ColumnCount++;
+
+            myPanel.ColumnStyles.Add(new ColumnStyle(colStyle.SizeType, colStyle.Width));
+           
+        }
+
+        private void btnDelRow_Click(object sender, EventArgs e)
+        {
+            if (myPanel.RowCount > 1)
+            {
+                RowStyle rowStyle = myPanel.RowStyles[myPanel.RowCount - 1];
+
+                myPanel.RowCount--;
+
+                myPanel.RowStyles.RemoveAt(myPanel.RowCount - 1);
+            }
+        }
+
+        private void btnAddRow_Click(object sender, EventArgs e)
+        {
+            RowStyle rowStyle = myPanel.RowStyles[myPanel.RowCount - 1];
+
+            myPanel.RowCount++;
+
+            myPanel.RowStyles.Add(new RowStyle(rowStyle.SizeType, rowStyle.Height));
+        }
+
+        private void myPanel_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            Point p = new Point(e.Row, e.Column);
+
+
+            if (cellColors.ContainsKey(p))
+            {
+                e.Graphics.FillRectangle(new SolidBrush(cellColors[p]), e.CellBounds);
+            }
+        }        
+
+        private void myPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point? clickedCell = GetRowColIndex(myPanel, e.Location);
+
+            if (clickedCell != null)
+            {
+                if (cellColors.ContainsKey(clickedCell.Value))
+                {
+                    cellColors[clickedCell.Value] = GetBlendedColor();
+                }
+                else
+                {
+                    cellColors.Add(clickedCell.Value, GetBlendedColor());
+                }
+                myPanel.Refresh();
+            }
+        }
+
+        private Color GetBlendedColor()
+        {
+            Color color1 = pnlColor1.BackColor;
+            Color color2 = pnlColor2.BackColor;
+
+            double ratio = 0.5;
+
+            if (color1.Name.ToLower() == "transparent")
+                ratio = 1;
+            else if (color2.Name.ToLower() == "transparent")
+                ratio = 0;
+
+            return Utility.Blend(color2, color1, ratio);
+        }
+
+
+
+        Point? GetRowColIndex(TableLayoutPanel tlp, Point point)
+        {
+            if (point.X > tlp.Width || point.Y > tlp.Height)
+                return null;
+
+            int w = tlp.Width;
+            int h = tlp.Height;
+            int[] widths = tlp.GetColumnWidths();
+
+            int i;
+            for (i = widths.Length - 1; i >= 0 && point.X < w; i--)
+                w -= widths[i];
+            int col = i + 1;
+
+            int[] heights = tlp.GetRowHeights();
+            for (i = heights.Length - 1; i >= 0 && point.Y < h; i--)
+                h -= heights[i];
+
+            int row = i + 1;
+
+            return new Point(row, col);
+        }
+
+        private void cbColor1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pnlColor1.BackColor = GetColor(cbColor1.SelectedItem.ToString());
+        }
+              
+        private void cbColor2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            pnlColor2.BackColor = GetColor(cbColor2.SelectedItem.ToString());
+        }
+
+        private Color GetColor(string color)
+        {
+            switch (color.ToLower())
+            {
+                case "red": return Color.Red;
+                case "green": return Color.Green;
+                case "yellow": return Color.Yellow;
+                case "black": return Color.Black;
+                case "white": return Color.White;
+            }
+            return Color.Transparent;
+        }
+
+        private void btnFinalize_Click(object sender, EventArgs e)
+        {
+            myPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.None;
+        }
+    }
+}
